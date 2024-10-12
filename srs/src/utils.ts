5d7be780233d57ebf1e9a53f05003505a5f25cc4
na <na>
2024-10-12 13:05:03 -0700

    modify this to construct a Blob from AudioStream and return it

--- srs/src/utils.ts
+++ srs/src/utils.ts
@@ -1,5 +1,52 @@
+import { OutputFormat, PollyClient, SynthesizeSpeechCommand, VoiceId } from "@aws-sdk/client-polly";
 import { TranslateClient, TranslateTextCommand } from "@aws-sdk/client-translate";
 import { readTextFile, remove } from "@tauri-apps/plugin-fs";
+
+const maleVoicesByLanguage: { [key: string]: string } = {
+  "ar": "Zeina",    // Arabic (although Zeina is female, it's the only Arabic voice)
+  "cmn": "Zhiyu",   // Chinese Mandarin
+  "da": "Mads",     // Danish
+  "nl": "Ruben",    // Dutch
+  "en": "Matthew",  // English (you might want to specify en-US, en-GB, etc.)
+  "fr": "Mathieu",  // French
+  "de": "Hans",     // German
+  "hi": "Aditi",    // Hindi (although Aditi is female, it's the only Hindi voice)
+  "is": "Karl",     // Icelandic
+  "it": "Giorgio",  // Italian
+  "ja": "Takumi",   // Japanese
+  "ko": "Seoyeon",  // Korean (although Seoyeon is female, it's the only Korean voice)
+  "nb": "Liv",      // Norwegian (although Liv is female, it's the only Norwegian voice)
+  "pl": "Jacek",    // Polish
+  "pt": "Cristiano",// Portuguese
+  "ro": "Carmen",   // Romanian (although Carmen is female, it's the only Romanian voice)
+  "ru": "Maxim",    // Russian
+  "es": "Enrique",  // Spanish
+  "sv": "Astrid",   // Swedish (although Astrid is female, it's the only Swedish voice)
+  "tr": "Filiz",    // Turkish (although Filiz is female, it's the only Turkish voice)
+  "cy": "Gwyneth",  // Welsh (although Gwyneth is female, it's the only Welsh voice)
+};
+
+export const getSpeechFromText = async (client: PollyClient, text: string, language: string) => {
+  const params = {
+    Text: text,
+    OutputFormat: OutputFormat.MP3,
+    VoiceId: maleVoicesByLanguage[language] as VoiceId
+  };
+
+  try {
+    const command = new SynthesizeSpeechCommand(params);
+    const data = await client.send(command);
+
+
+    if (data.AudioStream instanceof Buffer) {
+      const audioBlob = new Blob([data.AudioStream], { type: 'audio/mpeg' });
+      return audioBlob;
+    }
+
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 
 export const getTranslation = async (
   client: TranslateClient, 
