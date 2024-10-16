afefc7367de784a6758f65ae6dc2d47f320ccd30
na <na>
2024-10-16 16:07:49 -0700

    DUMP

--- srs/src/main.rs
+++ srs/src/main.rs
@@ -1,11 +1,13 @@
-use glib::clone;
+use anthropic::client::ClientBuilder;
+use anthropic::types::{ContentBlock, Message, MessagesRequestBuilder, Role};
 use gtk::gdk::Display;
-use gtk::{prelude::*, Button};
-use gtk::{Application, ApplicationWindow, TextView, Video, Box as GtkBox, Orientation, CssProvider, TextBuffer};
+use gtk::prelude::*;
+use gtk::{Application, ApplicationWindow, Video, Box as GtkBox, Orientation, CssProvider, Label};
 use gio::File;
 use serde::{Deserialize, Serialize};
 use std::cell::Cell;
 use std::fs;
+use std::env;
 use std::path::PathBuf;
 use rand::thread_rng;
 use rand::seq::SliceRandom;
@@ -17,13 +19,45 @@ struct Segment {
     language: String,
 }
 
+async fn get_translation(
+    text: &str, 
+    source_language: &str,
+    target_language: &str,
+) -> String {
+    let api_key = match env::var("ANTHROPIC_API_KEY") {
+        Ok(api_key) => api_key,
+        Err(_) => panic!("ANTHROPIC_API_KEY not set."),
+    };
+
+    let client = ClientBuilder::default().api_key(api_key).build()?;
+
+    let message = Message { 
+        role: Role::User, 
+        content: vec![ContentBlock::Text { text: "hello".to_string() }]
+    };
+    let request = MessagesRequestBuilder::default()
+        .model("claude-3-haiku-20240307".to_string())
+        .max_tokens::<u16>(400)
+        .stream(false)  
+        .messages(vec![message])
+        .build().unwrap();
+
+    let response = client.messages(request).await.unwrap();
+    match response.content.get(0).unwrap() {
+        ContentBlock::Text { text } => text.to_string(),
+        _ => panic!("Not text.")
+    }
+}
+
 fn main() {
     let app = Application::builder()
         .application_id("com.tongues.srs")
         .build();
 
     app.connect_startup(|_| load_css());
-    app.connect_activate(build_ui);
+    app.connect_activate(move |app| {
+        build_ui(app);
+    });
     app.run();
 }
 
@@ -68,44 +102,44 @@ fn build_ui(app: &Application) {
     video.set_file(Some(&file));
     video.set_autoplay(true);
 
-
-rust
+    let word_box = GtkBox::new(Orientation::Horizontal, 10);
+    word_box.add_css_class("text-xl");
     let words = values_vec[segment_index.get()].text.split(' ');
     for word in words {
-        println!("{}", word);
+        let word_label = Label::new(Some(word));
+        word_box.append(&word_label);
     }
 
-    let subtitles_buffer = TextBuffer::new(None);
-    subtitles_buffer.set_text(&values_vec[segment_index.get()].text);
-    let subtitles_view = TextView::with_buffer(&subtitles_buffer);
-    subtitles_view.add_css_class("text-xl");
+    // let next_button = Button::with_label("Next");
+    // next_button.connect_clicked(clone!(
+    //     #[strong]
+    //     word_box,
+    //     #[strong]
+    //     video,
+    //     move |_| {
+    //         segment_index.set(segment_index.get()+1);
 
-    let next_button = Button::with_label("Next");
-    next_button.connect_clicked(clone!(
-        #[strong]
-        subtitles_view,
-        #[strong]
-        video,
-        move |_| {
-            segment_index.set(segment_index.get()+1);
-            let buffer = TextBuffer::new(None);
-            buffer.set_text(&values_vec[segment_index.get()].text);
-            subtitles_view.set_buffer(Some(&buffer));
-            let file = File::for_path(&values_vec[segment_index.get()].media_path);
-            video.set_file(Some(&file));
-        }
-    ));
+    //         let words = values_vec[segment_index.get()].text.split(' ');
+    //         for word in words {
+    //             let buffer = TextBuffer::builder().text(word).build();
+    //             let text_view = TextView::builder().buffer(&buffer).editable(false).build();
+    //             word_box.append(&text_view);
+    //         }
+    //         let file = File::for_path(&values_vec[segment_index.get()].media_path);
+    //         video.set_file(Some(&file));
+    //     }
+    // ));
 
     let main_box = GtkBox::new(Orientation::Vertical, 5);
     main_box.append(&video);
-    main_box.append(&subtitles_view);
-    main_box.append(&next_button);
+    main_box.append(&word_box);
+    // main_box.append(&next_button);
 
     let window = ApplicationWindow::builder()
         .application(app)
         .title("SRS")
         .default_width(800)
-        .default_height(600)
+        .default_height(800)
         .child(&main_box)
         .build();
 
